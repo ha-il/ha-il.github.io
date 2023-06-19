c28af1adfe341d1471975323807a73444cc88a2c
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-06-19 15:53:34 +0900

    fix: Add category prop to fix header title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const Layout = ({ location, title, children }) => {
+const Layout = ({ location, title, category, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
+  const categoryPath = `${__PATH_PREFIX__}/categories/${category}/`
+  const isCategoryPath = location.pathname === categoryPath
+
   let header
 
-  if (isRootPath) {
+  if (isRootPath || isCategoryPath) {
     header = (
       <h1 className="main-heading">
         <Link to="/">{title}</Link>
